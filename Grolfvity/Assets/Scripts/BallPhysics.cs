0e9758476f1f37d725aad1dc9eb2295e9cb64af1
LinkedBlades <pablo.99.garcia.sachetti@gmail.com>
2024-12-27 03:11:55 -0300

    Aim line fix
    -Aim line now drawing properly.
    -Max line distance added (default value = 10). Can be changed in inspector

--- Grolfvity/Assets/Scripts/BallPhysics.cs
+++ Grolfvity/Assets/Scripts/BallPhysics.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEditor.Timeline.Actions;
 using UnityEditorInternal;
 using UnityEngine;
@@ -16,18 +17,20 @@ public class BallPhysics : MonoBehaviour
     //[SerializeField] float friction;
 
     //Shot variables
-    Vector2 mousePosIni;
     Vector2 mousePosEnd;
     Vector2 ballPos;
-    [SerializeField] Material mat;
     [SerializeField] float shotStrength = 1;
 
     //Aim Line variables
     Vector2 aimLineIni;
     Vector2 aimLineEnd;
+<<<<<<< Updated upstream
     //GameObject aimLine;
     //LineRenderer currentLineRenderer;
 
+=======
+    [SerializeField] float maxLineLenght = 10;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
@@ -41,11 +44,6 @@ public class BallPhysics : MonoBehaviour
     void FixedUpdate()
     {
 
-        if (mousePosEnd != null && ballPos != null)
-        {
-            Debug.DrawLine(ballPos, -mousePosEnd, Color.blue);
-
-        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -69,6 +67,7 @@ public class BallPhysics : MonoBehaviour
 
     private void OnMouseDrag()
     {
+<<<<<<< Updated upstream
         //Getting mouse position into world coordinates for aiming shot
         mousePosEnd = Input.mousePosition;
         mousePosEnd = Camera.main.ScreenToWorldPoint(mousePosEnd);
@@ -80,6 +79,37 @@ public class BallPhysics : MonoBehaviour
         if (Vector2.Distance(aimLineIni, mousePosEnd) > 1.0f)
         {
             aimLine.UpdateLineRenderer(ballPos, aimLineEnd);
+=======
+
+        //Variables for storing direction and lengtt of aim line
+        Vector2 aimDirection;
+        float aimMagnitude;
+
+        //Ball stopped check
+        if (rbody.velocity.magnitude <= 0.1f)
+        {
+            //Getting mouse position into world coordinates for aiming shot
+            mousePosEnd = Input.mousePosition;
+            mousePosEnd = Camera.main.ScreenToWorldPoint(mousePosEnd);
+
+            //Calculating direction and lenght of aimline based on ball and mouse position
+            aimDirection = (ballPos - mousePosEnd).normalized;
+            aimMagnitude = MathF.Min((ballPos - mousePosEnd).magnitude, maxLineLenght);
+
+            //Updating aim line end
+            aimLineEnd = ballPos + aimDirection * aimMagnitude;
+
+            //Checking for mouse distance before drawing aimline
+            if (Vector2.Distance(aimLineIni, mousePosEnd) > 1.0f)
+            {
+                aimLine.UpdateLineRenderer(ballPos, aimLineEnd);
+            }
+
+            if (Vector2.Distance(aimLineIni, mousePosEnd) <= 1.0f)
+            {
+                aimLine.ClearAimLine();
+            }
+>>>>>>> Stashed changes
         }
 
         if (Vector2.Distance(aimLineIni, mousePosEnd) <= 1.0f)
