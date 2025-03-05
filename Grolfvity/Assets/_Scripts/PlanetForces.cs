dd52b5b89d24f4b608854f3acb8d05262784346d
LinkedBlades <pablo.99.garcia.sachetti@gmail.com>
2025-03-04 22:49:00 -0300

    Code cleanup
    Cleaned up unused code in PlanetForces.cs
    Deleted BallOutOfBounds.cs since its implemented inside BallBehaviour.cs

--- Grolfvity/Assets/_Scripts/PlanetForces.cs
+++ Grolfvity/Assets/_Scripts/PlanetForces.cs
@@ -88,18 +88,16 @@ public class PlanetForces : MonoBehaviour
         Vector2 difference = this.transform.position - ballTransform.position;
         float distToBall = difference.magnitude;
 
-        //float distanceModifier = Vector3.Distance(this.transform.position, ballTransform.position);
-
-        //F = G * (m1*m2) / r^2
         float distFactor; // How much to scale down the pull foce based on distance
         
-        //Remapping force scaling based on distance
         if(distCap == 0)
         {
+            //F = G * (m1*m2) / r^2
             distFactor =  Mathf.Pow(distToBall, 2);
         }
         else
         {
+            //Remapping force scaling based on distance
             float temporary = Mathf.InverseLerp(oldRangeMax, oldRangeMin, Mathf.Pow(distToBall, 2));
             distFactor = Mathf.Lerp(newRangeMax, newRangeMin, temporary);
         }
@@ -107,11 +105,31 @@ public class PlanetForces : MonoBehaviour
         float forceMagnitude = pullMagnitude / distFactor;
 
         Vector2 pullDirection = difference.normalized;
-        //Vector2 pullDirection = Vector3.Normalize(this.transform.position - ballTransform.position);
         Vector2 gravVector = forceMagnitude * pullDirection;
 
         Debug.DrawRay(ballTransform.position, gravVector, Color.yellow);
 
         ballRb.AddForce(gravVector);
     }
+
+    private void GravityPull2(Rigidbody2D ballRb, Transform ballTransform)
+    {
+        //Distance between objects transforms
+        Vector2 difference = this.transform.position - ballTransform.position;
+        float distToBall = difference.magnitude;
+
+        //Testing using r instead of r^2
+        //F = G * (m1*m2) / r 
+
+        float forceMagnitude = pullMagnitude / distToBall;
+
+        Vector2 pullDirection = difference.normalized;
+        Vector2 gravVector = forceMagnitude * pullDirection;
+
+        Debug.DrawRay(ballTransform.position, gravVector, Color.yellow);
+
+        ballRb.AddForce(gravVector);
+    }
+
+
 }
